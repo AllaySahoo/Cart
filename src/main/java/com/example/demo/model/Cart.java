ce833ef47ee1bccd8878ddf26b764775e8ed453a
Allay Sahoo <allaysahoo@gmail.com>
2024-02-01 12:42:18 +0530

    sa

--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -2,15 +2,12 @@ package com.example.demo.model;
 
 import java.math.BigDecimal;
 import java.util.Date;
-
-import org.apache.tomcat.jni.Library;
-
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,41 +16,21 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int cartId;
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int dishId;
-
-    
-    // private int userId;
-    // private int hotelId;
-    
-
+    @OneToMany(targetEntity=Dish.class)  
+    private List dishes;
     private Date date;
     private int quantity;
     private BigDecimal estimatedPrice;
 
-    // @OneToOne
-    // private User user;
-
-    // @OneToOne
-    // private Hotel hotel;
-
-    // @OneToMany(targetEntity=Dish.class)  
-    // private Dish dish;
+    public List getDishes() {  
+        return dishes;  
+    }  
+    public void setDishes(List dishes) {  
+        this.dishes = dishes;  
+    }
 
     public int getCartId() {
         return cartId;
-    }
-
-    // public int getUserId() {
-    //     return userId;
-    // }
-
-    // public int getHotelId() {
-    //     return hotelId;
-    // }
-
-    public int getDishId() {
-        return dishId;
     }
 
     public Date getDate() {
@@ -71,11 +48,7 @@ public class Cart {
     public void setDate(Date date) {
         this.date = date;
     }
-
-    public void setDishId(int dishId) {
-        this.dishId = dishId;
-    }
-
+        
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
---
+++ src/main/java/com/example/demo/model/Dish.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "dish")
+public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int dishId;    
+    private String name;
+    private BigDecimal price;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
